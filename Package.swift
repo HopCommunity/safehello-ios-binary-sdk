1b17f6f4c6e371b29dffe2664988dbde801f5539
Eric Hexter <eric@safehello.com>
2022-07-26 10:52:38 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SafeHelloSDK",
-            url: "https://github.com/SafeHello/safehello-ios-binary-sdk/releases/download/1.0.0/SafeHelloSDK.zip",
+            url: "https://github.com/SafeHello/safehello-sdk-ios/releases/download/1.0.0/SafeHelloSDK.zip",
             checksum: "7d97d641b211e2bd9e579271c8c483d80eca0d7130303439f76ad997c5e1b1f8"
         )
     ]
